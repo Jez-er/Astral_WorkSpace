d9f01dfd3a7c462e6213a7301b096ebb1a33d60b
Jez-er <andrey.chernenko008@gmail.com>
2024-08-22 15:49:16 +0300

    [ADD] Geting user data

--- Front-end/src/App/app.tsx
+++ Front-end/src/App/app.tsx
@@ -1,16 +1,25 @@
+import Cookies from 'js-cookie'
 import { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router'
 import { useUserStore } from './store/User.store'
 
 const App = () => {
-
 	const refresh = useUserStore(state => state.refresh)
+	const getUserData = useUserStore(state => state.getUserData)
+	const mySession = Cookies.get('mySession')
+	const user = useUserStore(state => state.user)
+	const isAuth = useUserStore(state => state.isAuth)
 
 	useEffect(() => {
-		refresh()
+		if (mySession?.length !== 0 && Object.keys(user).length === 0) {
+			getUserData()
+		}
+		if (isAuth) {
+			refresh()
+		}
 	}, [])
-	return (<RouterProvider router={router} />)
+	return <RouterProvider router={router} />
 }
 
 export default App
