b4950bbd3d3bf3cb9bf7a303ed2682a47edeac35
Jez-er <andrey.chernenko008@gmail.com>
2024-08-22 15:50:27 +0300

    [UPDATE] Router

--- Front-end/src/App/router/router.tsx
+++ Front-end/src/App/router/router.tsx
@@ -1,32 +1,39 @@
-import { createBrowserRouter } from "react-router-dom"
-import ForgotPass from "../../pages/auth/Forgot_pass"
-import Login from "../../pages/auth/login"
-import Registration from "../../pages/auth/registration"
+import { createBrowserRouter } from 'react-router-dom'
+import MainLayout from '../../layouts/mainlayout'
+import ForgotPass from '../../pages/auth/Forgot_pass'
+import Login from '../../pages/auth/login'
+import Registration from '../../pages/auth/registration'
 import WorkSpaces from '../../pages/workspaces'
 
 const router = createBrowserRouter([
-    {
-        path: '/auth',
-        children: [
-            {
-                path: 'login',
-                element: <Login />,
-                index: true
-            },
-            {
-                path: 'password/reset',
-                element: <ForgotPass />
-            },
-            {
-                path: 'registration',
-                element: <Registration />
-            }
-        ]
-    },
-    {
-        path: '/workspaces',
-        element: <WorkSpaces />
-    }
-]);
+	{
+		path: '/auth',
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+				index: true,
+			},
+			{
+				path: 'password/reset',
+				element: <ForgotPass />,
+			},
+			{
+				path: 'registration',
+				element: <Registration />,
+			},
+		],
+	},
+	{
+		path: '/',
+		element: <MainLayout />,
+		children: [
+			{
+				path: 'workspaces',
+				element: <WorkSpaces />,
+			},
+		],
+	},
+])
 
-export default router;
+export default router
